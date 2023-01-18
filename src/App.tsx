8fc32ed04c9c47afba9a7a9870403cbe31ab0d7e
sebastianhawlitzki <business.hawlitzki@gmail.com>
2023-01-18 16:05:32 +0100

    update: sign up, login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import './App.css';
-import SignUpPage from "./pages/SignUpPage";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/HomePage";
-import Auth from "./components/Auth";
+import {BrowserRouter} from "react-router-dom";
+import Root from "./Root";
 
-export default function  App() {
-  return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<SignUpPage/>}/>
-          <Route path="/login" element={<LoginPage/>}/>
-          <Route path="/" element={
-            <Auth>
-              <HomePage/>
-            </Auth>
-          }/>
-        </Routes>
-      </BrowserRouter>
-  );
+function App() {
+    return (
+        <BrowserRouter>
+            <Root/>
+        </BrowserRouter>
+    );
 }
 
+export default App;
