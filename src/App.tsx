a8e5420e619a29fe4b7154307a4ef9a903d5387a
sebastianhawlitzki <business.hawlitzki@gmail.com>
2023-01-18 12:28:24 +0100

    init: BIG BANG

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignUpPage from "./pages/SignUpPage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import Auth from "./components/Auth";
 
-function App() {
+export default function  App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUpPage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/" element={
+            <Auth>
+              <HomePage/>
+            </Auth>
+          }/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
-export default App;
